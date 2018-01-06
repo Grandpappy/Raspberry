34ca7b6a7dfa986c704cfa51afe22635c53c9334
Grandpappy <grandpappy@gmail.com>
2018-01-05 20:19:27 -0600

    Added combo and coop cards

--- all-player-decks.rb
+++ all-player-decks.rb
@@ -2,6 +2,11 @@ require 'squib'
 
 data = Squib.xlsx file: 'data/all-player-decks.xlsx'
 layouts = ['all-decks-layout.yml', 'all-player-decks-layout.yml', 'card-base-types.yml']
+comboTriggers = data['ComboTrigger'].map { |e| e.nil? ? 'none' : e }
+comboBoxes = data['ComboTrigger'].map { |e| e.nil? ? 'none' : 'combo_box' }
+comboLine = data['ComboTrigger'].map { |e| e.nil? ? 'none' : 'combo_divider_line' }
+cooperative = data['IsCoop'].map { |e| e.nil? ? 'none' : 'cooperative' }
+
 #sub_deck = data['SubDeck'] { |e| e.nil? ? data['BaseType'][i] : e }
 
 Squib::Deck.new(cards: data['Name'].size, layout: layouts, width: 825, height: 1125) do
@@ -11,7 +16,8 @@ Squib::Deck.new(cards: data['Name'].size, layout: layouts, width: 825, height: 1
   rect layout: 'cut' # cut line as defined by TheGameCrafter
   rect layout: 'safe' # safe zone as defined by TheGameCrafter
 
-
+  svg layout: data['BaseType']
+  svg layout: cooperative
   text str: data['Name'], layout: 'title'
 
   #, hint: :red
@@ -27,14 +33,32 @@ Squib::Deck.new(cards: data['Name'].size, layout: layouts, width: 825, height: 1
     embed.svg key: ':corruption:', file: 'icons\\death-skull.svg', layout: 'embedded_icon'
   end
 
-  text str: data['Cost'], layout: 'cost'
-  text str: data['Deck'], layout: 'deck_name'
+  # rect layout: 'combo_box'
+  rect layout: comboBoxes
+  line layout: comboLine
 
+  #, hint: :red
+  text str: data['ComboEffect'], layout: 'combo_effect' do |embed|
+    embed.svg key: ':strike:', file: 'icons\\revolt.svg', layout: 'embedded_icon'
+    embed.svg key: ':coin:', file: 'icons\\crown-coin.svg', layout: 'embedded_icon'
+    embed.svg key: ':wound:', file: 'icons\\thunder-skull.svg', layout: 'embedded_icon'
+    embed.svg key: ':heal:', file: 'icons\\heart-plus.svg', layout: 'embedded_icon'
+    embed.svg key: ':draw:', file: 'icons\\card-draw.svg', layout: 'embedded_icon'
+    embed.svg key: ':move:', file: 'icons\\run.svg', layout: 'embedded_icon'
+    embed.svg key: ':shield:', file: 'icons\\checked-shield.svg', layout: 'embedded_icon'
+    embed.svg key: ':heart:', file: 'icons\\heart-organ.svg', layout: 'embedded_icon'
+    embed.svg key: ':corruption:', file: 'icons\\death-skull.svg', layout: 'embedded_icon'
+  end
+
+  svg layout: comboTriggers, y: 800, x: 125
+
+  text str: data['Cost'], layout: 'cost'
+
+  text str: data['Deck'], layout: 'deck_name'
   text str: Time.now, layout: 'credits'
 
-  svg layout: data['BaseType']
 
-  # save_png prefix: 'player_'
-  save_pdf file: 'all_player_decks.pdf', trim: 37.5 #, sprue: 'a4_poker_card_8up.yml'
+  save_png prefix: 'player_'
+  #save_pdf file: 'all_player_decks.pdf', trim: 37.5 #, sprue: 'a4_poker_card_8up.yml'
 
 end
